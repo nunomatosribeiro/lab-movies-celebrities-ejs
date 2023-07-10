396a5442e2ce1057a57a3f3609bbe424ffba1f72
unknown <nunoribeiroft@outlook.com>
2023-07-10 16:08:12 +0100

    iteration 5

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,37 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const Celebrity = require("../models/Celebrity.model");
+// all your routes here
+
+router.get("/create", (req, res) => {
+ 
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/create", async (req, res) => {
+  const celebrity = req.body;
+
+  try {
+    const newCelebrity = await Celebrity.create(celebrity);
+    res.redirect('/celebrities')
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get('/celebrities', async (req, res) =>{
+    
+try{
+    const allCelebrities = await Celebrity.find()
+    res.render('celebrities/celebrities', {allCelebrities})
+}catch(error){
+    console.log(error)
+}
+    
+})
+
+
+
+module.exports = router;
